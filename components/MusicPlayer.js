db33f3a665049f3d91257f95fac07d3d2f49c63a
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-08-14 14:07:47 +0300

    react-native-slider

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const MusicPlayer = () => {
   return (
-    <View style={styles.container}>
-      <Text>React Native Music Player</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainContainer}>
+        <Ionicons name='heart-outline' size={45} />
+        <Text>React Native Music Player</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -14,7 +18,11 @@ export default MusicPlayer
 let styles=StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#222831'
+  },
+  mainContainer: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   }
 })
